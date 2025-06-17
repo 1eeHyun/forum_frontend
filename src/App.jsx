3042f7f0cd33d2a73ac251e3e4df948eb3311827
1eeHyun <dlee2006@uic.edu>
2025-06-17 13:00:19 -0500

    Add basic directories

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ChatProvider } from "@/context/ChatContext";
-import { AuthProvider } from "@/context/AuthContext";
-
-import Layout from "@/components/Layout";
-import AppRoutes from "@/routes/AppRoutes";
-import PingOnlineStatus from "@/components/system/PingOnlineStatus";
+import HomePage from "@/features/home/pages/HomePage";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <ChatProvider>
-        <AuthProvider>
-          <PingOnlineStatus />
-          <Routes>
-            <Route element={<Layout />}>
-              <Route path="*" element={<AppRoutes />} />
-            </Route>
-          </Routes>
-        </AuthProvider>
-      </ChatProvider>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </BrowserRouter>
   );
 }
