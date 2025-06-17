1d1d41f353350347df376e82f727bc14697443c2
1eeHyun <dlee2006@uic.edu>
2025-06-17 14:32:32 -0500

    feat: update App routing to include HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Navbar from "@/layout/Navbar";
-import HomePage from "@home/pages/HomePage";
+import AppRoutes from "@/routes/AppRoutes";
 
 export default function App() {
   return (
     <BrowserRouter>
       <div className="bg-zinc-900 text-white min-h-screen">
-        <Navbar /> 
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-        </Routes>
+        <Navbar />
+        <AppRoutes />
       </div>
     </BrowserRouter>
   );
